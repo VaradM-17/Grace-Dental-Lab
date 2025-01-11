51496574139988a8de4c28c2fb9a1dbaa8b06347
osb15 <ombhamare51@gmail.com>
2025-01-11 13:24:22 +0530

    Entity and dao for appoinment added

---
+++ src/main/java/com/gdl/GDL/Dao/AppointmentDao.java
@@ -0,0 +1,47 @@
+package com.gdl.GDL.Dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.gdl.GDL.Entity.Appointment;
+
+@Repository
+public class AppointmentDao {
+	@Autowired
+	private SessionFactory sf;
+
+	public String bookappointment(Appointment app) {
+		Session ss = null;
+		Transaction tr = null;
+		String msg = null;
+
+		try {
+			ss = sf.openSession();
+			tr = ss.beginTransaction();
+			ss.persist(app);
+			msg = "Your appointment has been successfully booked.";
+			tr.commit();
+
+		} catch (Exception e) {
+
+			if (tr != null) {
+
+				tr.rollback();
+
+			}
+			e.printStackTrace();
+		} finally {
+
+			if (ss != null) {
+				ss.close();
+			}
+
+		}
+		return msg;
+
+	}
+
+}
---
+++ src/main/java/com/gdl/GDL/Entity/Appointment.java
@@ -0,0 +1,80 @@
+package com.gdl.GDL.Entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Appointment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long appointmentId;
+	private LocalDate appointmentDate;
+	private LocalTime appointmentTime;
+
+	private String patientName;
+	private Integer patientAge;
+
+	public Appointment() {
+
+	}
+
+	public Appointment(Long appointmentId, LocalDate appointmentDate, LocalTime appointmentTime, String patientName,
+			Integer patientAge) {
+		super();
+		this.appointmentId = appointmentId;
+		this.appointmentDate = appointmentDate;
+		this.appointmentTime = appointmentTime;
+		this.patientName = patientName;
+		this.patientAge = patientAge;
+	}
+
+	public Long getAppointmentId() {
+		return appointmentId;
+	}
+
+	public void setAppointmentId(Long appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+	public LocalDate getAppointmentDate() {
+		return appointmentDate;
+	}
+
+	public void setAppointmentDate(LocalDate appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+	public LocalTime getAppointmentTime() {
+		return appointmentTime;
+	}
+
+	public void setAppointmentTime(LocalTime appointmentTime) {
+		this.appointmentTime = appointmentTime;
+	}
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public Integer getPatientAge() {
+		return patientAge;
+	}
+
+	public void setPatientAge(Integer patientAge) {
+		this.patientAge = patientAge;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [appointmentId=" + appointmentId + ", appointmentDate=" + appointmentDate
+				+ ", appointmentTime=" + appointmentTime + ", patientName=" + patientName + ", patientAge=" + patientAge
+				+ "]";
+	}
+
+}
